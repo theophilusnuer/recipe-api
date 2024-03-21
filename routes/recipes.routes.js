60f522391e3573def2c0ee042dc3fa77302c489a
theophilusnuer <tphilus003@gmail.com>
2024-03-21 08:07:54 +0000

    edited the routes

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,25 +1,31 @@
 import { Router } from "express";
+import { Recipes } from "../modules/recipes.js";
 
 const router = Router();
 
-router.post("/recipes", (req,res)=>{
-res.send("Add recipes");
+router.post("/recipes", async (req,res)=>{
+const results = await Recipes.create();
+res.json(results);
 });
 
-router.get("/recipes", (req, res) => {
-    res.send("Get recipes!");
+router.get("/recipes", async (req, res) => {
+  const results = await Recipes.find();
+  res.json(results);
   });
 
-  router.get("/recipes/:id", (req, res) => {
-    res.send("Get a single recipe!");
+  router.get("/recipes/:id", async (req, res) => {
+    const results = await Recipes.findOne();
+    res.json(results);
   });
   
-  router.patch("/recipes/:id", (req, res) => {
-      res.send("Update a single recipe!");
+  router.patch("/recipes/:id", async (req, res) => {
+    const results = await Recipes.updateOne();
+    res.json(results);
   });
   
-  router.delete("/recipes/:id", (req, res) => {
-    res.send("Delete a single recipe!");
+  router.delete("/recipes/:id", async (req, res) => {
+    const results = await Recipes.deleteOne();
+    res.json(results);
   });
 
 export default router;
