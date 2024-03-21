df8e1095b3d0432bdc7c96d6e6154f4343d47daa
theophilusnuer <tphilus003@gmail.com>
2024-03-21 10:43:28 +0000

    update on controllers

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,26 +1,26 @@
 import { RecipeModel } from "../models/recipe.js";
 
 export const addRecipe = async (req, res) => {
-  const results = await RecipeModel.create();
+  const results = await RecipeModel.create(req.body);
   res.json(results);
 };
 
 export const getRecipes = async (req, res) => {
-  const results = await RecipeModel.find();
+  const results = await RecipeModel.find(req.body);
   res.json(results);
 };
 
 export const getRecipe = async (req, res) => {
-  const results = await RecipeModel.findOne();
+  const results = await RecipeModel.findOne(req.body);
   res.json(results);
 };
 
 export const updateRecipe = async (req, res) => {
-  const results = await RecipeModel.updateOne();
+  const results = await RecipeModel.updateOne(req.body);
   res.json(results);
 };
 
 export const deleteRecipe = async (req, res) => {
-  const results = await RecipeModel.deleteOne();
+  const results = await RecipeModel.deleteOne(req.body);
   res.json(results);
 };
--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { RecipeModel } from "../models/recipe.js";
 import {
   addRecipe,
   deleteRecipe,
